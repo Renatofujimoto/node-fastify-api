d93ba6f4d36e4cc011843566dc0b83971d50d543
Renatofujimoto <renatofujimoto2@gmail.com>
2024-02-14 08:49:31 -0300

    feat: creating get,vote and create poll, adding prisma and new migrate for database

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,19 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
-import z from "zod";
+import { createPool } from "./routes/create-poll";
+import { getPool } from "./routes/get-polls";
+import { voteOnPool } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post("/polls", async (request, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({ pollId: poll.id });
+app.register(cookie, {
+  secret: "my-secret",
+  hook: "onRequest",
 });
+
+app.register(createPool);
+app.register(getPool);
+app.register(voteOnPool);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP Server running on port:3333");
